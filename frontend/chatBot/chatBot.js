77cb82d09a24746e36c065f59da841dfd8c451e2
Prabh-Sekhar <pravangshusekhar09@gmail.com>
2025-02-11 22:03:08 +0530

    Update chatbot

--- frontend/chatBot/chatBot.js
+++ frontend/chatBot/chatBot.js
@@ -1,1 +1,28 @@
+let enterBtn = document.querySelector("#enter");
+let chatOutgoing = document.querySelector(".chat-outgoing");
+let message = document.querySelector("#message");
+let incomingChat = document.querySelector("#usertext");
+let userForm = document.querySelector("#userinput");
+
+let messageEnter = (event) => {
+    event.preventDefault();
+    if (message.value.trim() === "") return;
+
+    chatOutgoing.style.display = "flex";
+    incomingChat.innerText = message.value;
+    message.value = "";
+};
+
+message.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        messageEnter(event);
+    }
+});
+
+enterBtn.addEventListener("click", messageEnter);
+userForm.addEventListener("submit", messageEnter);
+
+
+
+
 
